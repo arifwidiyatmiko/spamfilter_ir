a64efdd7b92ef3fa5be4f650f84bf704afb69bd0
arifwidiyatmiko <arifwidiyatmiko@gmail.com>
2018-01-14 00:51:01 +0700

    add dataset,fixing

--- main.py
+++ main.py
@@ -6,8 +6,52 @@ from sklearn.svm import SVC, NuSVC, LinearSVC
 from sklearn.metrics import confusion_matrix
 # Create a dictionary of words with its frequency
 
-train_dir = 'train-mails'
+def make_Dictionary(train_dir):
+    emails = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]
+    all_words = []
+    for mail in emails:
+        with open(mail) as m:
+            for i, line in enumerate(m):
+                if i == 2:  # Body of email is only 3rd line of text file
+                    words = line.split()
+                    all_words += words
+
+    dictionary = Counter(all_words)
+    list_to_remove = dictionary.keys()
+    for item in list_to_remove:
+        if item.isalpha() == False:
+            del dictionary[item]
+        elif len(item) == 1:
+            del dictionary[item]
+    dictionary = dictionary.most_common(3000)
+    # Paste code for non-word removal here(code snippet is given below)
+    return dictionary
+
+
+
+def extract_features(mail_dir):
+    files = [os.path.join(mail_dir,fi) for fi in os.listdir(mail_dir)]
+    features_matrix = np.zeros((len(files),3000))
+    docID = 0;
+    for fil in files:
+      with open(fil) as fi:
+        for i,line in enumerate(fi):
+          if i == 2:
+            words = line.split()
+            for word in words:
+              wordID = 0
+              for i,d in enumerate(dictionary):
+                if d[0] == word:
+                  wordID = i
+                  features_matrix[docID,wordID] = words.count(word)
+        docID = docID + 1
+    return features_matrix
+
+
+train_dir = 'test-mails'
 dictionary = make_Dictionary(train_dir)
+
+
 
 # Prepare feature vectors per training mail and its labels
 
@@ -31,44 +75,4 @@ result1 = model1.predict(test_matrix)
 result2 = model2.predict(test_matrix)
 print confusion_matrix(test_labels,result1)
 print confusion_matrix(test_labels,result2)
-
-def make_Dictionary(train_dir):
-    emails = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]
-    all_words = []
-    for mail in emails:
-        with open(mail) as m:
-            for i, line in enumerate(m):
-                if i == 2:  # Body of email is only 3rd line of text file
-                    words = line.split()
-                    all_words += words
-
-    dictionary = Counter(all_words)
-    # Paste code for non-word removal here(code snippet is given below)
-    return dictionary
-
-list_to_remove = dictionary.keys()
-for item in list_to_remove:
-    if item.isalpha() == False:
-        del dictionary[item]
-    elif len(item) == 1:
-        del dictionary[item]
-dictionary = dictionary.most_common(3000)
-
-def extract_features(mail_dir):
-    files = [os.path.join(mail_dir,fi) for fi in os.listdir(mail_dir)]
-    features_matrix = np.zeros((len(files),3000))
-    docID = 0;
-    for fil in files:
-      with open(fil) as fi:
-        for i,line in enumerate(fi):
-          if i == 2:
-            words = line.split()
-            for word in words:
-              wordID = 0
-              for i,d in enumerate(dictionary):
-                if d[0] == word:
-                  wordID = i
-                  features_matrix[docID,wordID] = words.count(word)
-        docID = docID + 1
-    return features_matrix
 
