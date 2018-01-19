3ff1b201fc9d2441e29c25d7401bd5d1f24f48c8
arifwidiyatmiko <arifwidiyatmiko@gmail.com>
2018-01-20 01:33:53 +0700

    tambah stopwords

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
 from sklearn.svm import SVC, NuSVC, LinearSVC
 from sklearn.metrics import confusion_matrix
 
+stopword_list = ["a", "about" ,"above", "after", "again" ,"against" ,"all" ,"am" ,"an" ,"and", "any", "are", "aren't" ,"as" ,"at" ,"be", "because", "been" ,"before" ,"being" ,"below", "between", "both", "but", "by", "can't" ,"cannot" ,"could" ,"couldn't", "did" ,"didn't", "do" ,"does" ,"doesn't" ,"doing" ,"don't" ,"down", "during" ,"each", "few", "for" ,"from", "further", "had", "hadn't" ,"has" ,"hasn't", "have", "haven't", "having" ",he" ,"he'd", "he'll", "he's", "her", "here" ,"here's", "hers", "herself", "him", "himself" ,"his" ,"how", "how's", "i", "i'd" ,"i'll", "i'm", "i've", "if" ,"in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most" ,"mustn't" ,"my" ,"myself" ,"no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours","ourselves", "out", "over", "own", "same", "shan't", "she", "she'd" ,"she'll", "she's", "should", "shouldn't" ,"so", "some", "such", "than", "that", "that's" ,"the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where" ,"where's", "which", "while" ,"who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"];
 
 # Create a dictionary of words with its frequency
 
@@ -52,7 +53,7 @@ def extract_features(mail_dir):
 
 train_dir = 'ling-spam/train-mails'
 dictionary = make_Dictionary(train_dir)
-
+# print dictionary
 # Prepare feature vectors per training mail and its labels
 
 train_labels = np.zeros(702)
